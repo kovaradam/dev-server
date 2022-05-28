41a79694bb0b8224766da843b83a447ecea02314
Adam Kovar <kovaradam1@gmail.com>
2022-05-28 19:04:04 +0200

    put client script into ts file fix working directory path

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { mergeReadableStreams } from 'https://deno.land/std@0.126.0/streams/merge.ts';
-import { createArgumentMap, log } from './utils.ts';
+import { createArgumentMap, html, log } from './utils.ts';
 
 const args = createArgumentMap();
 const PORT = args['-p'] ?? 3000;
@@ -19,8 +19,6 @@ const server = Deno.listen({ port: Number(PORT) });
   }
 })();
 
-const WORKING_DIRECTORY = new URL('.', import.meta.url).pathname;
-
 const fsWatcherStore = {
   _isDirty: false,
   getIsDirty() {
@@ -34,7 +32,7 @@ const fsWatcherStore = {
 };
 
 (async () => {
-  const directory = `${WORKING_DIRECTORY}${SUBDIRECTORY}`;
+  const directory = `${Deno.cwd()}/${SUBDIRECTORY}`;
   const watcher = Deno.watchFs(directory);
 
   log.info(`Watching for file changes in ${log.Colors.bold(directory)}`);
@@ -74,7 +72,20 @@ async function handleHttp(connection: Deno.Conn) {
   }
 }
 
-const INJECT_SCRIPT = Deno.readTextFileSync('./client-script.html');
+const INJECT_SCRIPT = html`
+  <script>
+    const intervalId = setInterval(async () => {
+      const response = await fetch("/poll").catch((reason) => {
+        clearInterval(intervalId);
+        throw new Error("Dev server is unreachable");
+      });
+
+      if (response?.status === 205) {
+        window.location.reload();
+      }
+    }, 1000);
+  </script>
+`;
 
 function createFileResponseStream(file: Deno.FsFile, htmlSlice: string) {
   const textEncoderStream = new TextEncoderStream();
--- utils.ts
+++ utils.ts
@@ -1,17 +1,17 @@
-import * as Colors from "https://deno.land/std@0.141.0/fmt/colors.ts";
+import * as Colors from 'https://deno.land/std@0.141.0/fmt/colors.ts';
 
 export function createArgumentMap() {
-  const flags = ["-p", "-d", "-h"] as const;
+  const flags = ['-p', '-d', '-h'] as const;
   function isValidFlag(flag: string): flag is typeof flags[number] {
     return flags.includes(flag as typeof flags[number]);
   }
 
   const argumentMap = Object.fromEntries(
-    flags.map((flag) => [flag, undefined])
+    flags.map((flag) => [flag, undefined]),
   ) as Record<typeof flags[number], unknown | undefined>;
 
   Deno.args.forEach((argument, index, array) => {
-    if (argument === "-h") {
+    if (argument === '-h') {
       argumentMap[argument] = true;
       return;
     }
@@ -33,7 +33,7 @@ export function log(input: Parameters<typeof console.log>[number]) {
 }
 
 log.info = (input: Parameters<typeof console.info>[number]) => {
-  console.info(`${Colors.gray(">")} ${input}`);
+  console.info(`${Colors.gray('>')} ${input}`);
 };
 
 log.Colors = Colors;
@@ -41,35 +41,38 @@ log.Colors = Colors;
 log.fsEvent = (event: Deno.FsEvent) => {
   let color: keyof typeof Colors;
   switch (event.kind) {
-    case "access":
+    case 'access':
       return;
-    case "create":
-      color = "green";
+    case 'create':
+      color = 'green';
       break;
-    case "remove":
-      color = "red";
+    case 'remove':
+      color = 'red';
       break;
     default:
-      color = "yellow";
+      color = 'yellow';
       break;
   }
-  console.log(`${Colors[color](event.kind)} file ${event.paths}`);
+  console.log(`${Colors[color](event.kind)}: ${event.paths}`);
 };
 
 log.help = () => {
   console.table([
     {
-      argument: "-p",
-      description: "define port for server to listen to, default is 3000",
+      argument: '-p',
+      description: 'define port for server to listen to, default is 3000',
     },
     {
-      argument: "-d",
+      argument: '-d',
       description:
-        "specify directory to be watched for changes, default is current directory",
+        'specify directory to be watched for changes, default is current directory',
     },
     {
-      argument: "-h",
-      description: "show help",
+      argument: '-h',
+      description: 'show help',
     },
   ]);
 };
+
+// Enable syntax highlighting with bierner.lit-html extension, return input as is
+export const html = (value: TemplateStringsArray) => value[0];
